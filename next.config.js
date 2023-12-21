a8c360c97cc2a5d8619fb55d34c459aaf28f0623
Alexey Cherepanov <cherepanov.a@future-group.ru>
2023-12-22 00:53:01 +0400

    fix gitguardian warn; update config; add react-hook-form + addons

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+    dangerouslyAllowSVG: true,
+  }
+}
 
 module.exports = nextConfig
